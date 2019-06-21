c954f44b57026b7dceb6f4a6190a62438350f666
Daniel Hutt <dhutt@dragoonitcn.com>
2019-06-21 11:20:04 -0400

    Made various changes suggested in reddit comment

---
+++ WeaponStatGenerator/Models/GenerateNewWeapon.cs
@@ -0,0 +1,31 @@
+﻿namespace WeaponStatGenerator.Models
+{
+    public interface IGenerateNewWeapon
+    {
+        Weapon Generate(WeaponArchType weaponArchType);
+    }
+
+    public class GenerateNewWeapon : IGenerateNewWeapon
+    {
+        private readonly IGenerateDamageType _generateDamageType;
+        private readonly IGenerateDamageStat _generateDamageStat;
+        private readonly IGenerateWeaponName _generateWeaponName;
+
+        public GenerateNewWeapon(
+            IGenerateWeaponName generateWeaponName,
+            IGenerateDamageType generateDamageType,
+            IGenerateDamageStat generateDamageStat)
+        {
+            _generateDamageType = generateDamageType;
+            _generateDamageStat = generateDamageStat;
+            _generateWeaponName = generateWeaponName;
+        }
+
+        public Weapon Generate(WeaponArchType weaponArchType) =>
+            new Weapon(
+                _generateWeaponName.Generate(),
+                weaponArchType,
+                _generateDamageStat.Generate(),
+                _generateDamageType.Generate());
+    }
+}
